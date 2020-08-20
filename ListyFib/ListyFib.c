b21632e4adcd50a2d5f27e86968371f3aa1a3945
sam-mcginnis <sammcg96@aol.com>
2020-08-19 23:00:21 -0400

    a program to compute a fibonacci sequnce using linked lists

---
+++ ListyFib/ListyFib.c
@@ -0,0 +1,60 @@
+//Sam McGinnis CS1
+//Summer 2018
+//sa653767
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include "ListyFib.h"
+
+
+ListyInt *listyAdd(ListyInt *p, ListyInt *q)
+{
+    return NULL;
+}
+
+ListyInt *destroyListyInt(ListyInt *listy)
+{
+    return NULL;
+}
+
+ListyInt *stringToListyInt(char *str)
+{
+    return NULL;
+}
+
+char *listyIntToString(ListyInt *listy)
+{
+    return NULL;
+}
+
+ListyInt *uintToListyInt(unsigned int n)
+{
+    return NULL;
+}
+
+unsigned int *listyIntToUint(ListyInt *listy)
+{
+    return NULL;
+}
+
+void plusPlus(ListyInt *listy)
+{
+    return;
+}
+
+ListyInt *fib(unsigned int n)
+{
+    return NULL;
+}
+
+double difficultyRating(void)
+{
+    return 5.0;
+}
+
+double hoursSpent(void)
+{
+    return 8.5;
+}
